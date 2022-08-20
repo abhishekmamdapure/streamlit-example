48ad0b0c6c470d2e740df75ca0154c05dbb27219
Abhishek Mamdapure <mamdapureabhishek@gmail.com>
2022-08-20 20:21:23 +0530

    added ploty plot and custom buttons are now hidden

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,43 +4,50 @@ import math
 import pandas as pd
 import streamlit as st
 
+img = Image.open("wynum_02@2x.png")
+st.set_page_config(page_title="Wynum App", page_icon=img)
+
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden; }
+    footer {visibility: hidden;}
+    </style>
+    """
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 """
-# Welcome to Streamlit!
+# Welcome to Wynum!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+import plotly.graph_objects as go
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def create_trace(zoo_name,giraffes,orangutans,monkeys):
+    trace = go.Bar(
+    y=['giraffes', 'orangutans', 'monkeys'],
+    x=[giraffes, orangutans, monkeys],
+    name=zoo_name,
+    orientation='h',
+    marker=dict(
+        # color='rgba(246, 78, 139, 0.6)',
+        line=dict(color='black', width=3)
+        )
+    )
+    return trace
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+zoo = st.multiselect('Select Zoo Name', ['SF', 'MI', 'IL'])
+g = st.number_input(f'Number of giraffes', 0, 100)
+o = st.number_input(f'Number of orangutans', 0, 100)
+m = st.number_input(f'Number of monkeys', 0, 100)
 
-    points_per_turn = total_points / num_turns
+fig = go.Figure()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+for z in zoo:
+    fig.add_trace(create_trace(z,g,o,m))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-    
-    hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden; }
-        footer {visibility: hidden;}
-        </style>
-        """
-    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+fig.update_layout(barmode='stack')
+
+st.plotly_chart(fig, use_container_width=True)
+
